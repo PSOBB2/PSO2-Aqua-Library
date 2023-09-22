453b5eb5c69342b0fe7b69d179d222eb03db7edb
HelloKitty <andrew.blakely@ymail.com>
2023-09-21 22:52:37 -0500

    Added error logging for failed batch export

--- PSOBB2.ModelTool.FBXExporter/Program.cs
+++ PSOBB2.ModelTool.FBXExporter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Numerics;
+using System.Threading.Tasks;
 using AquaModelLibrary;
 using AquaModelLibrary.Native.Fbx;
 
@@ -16,7 +17,7 @@ namespace PSOBB2.ModelTool.FBXExporter
 		public static List<string> motionConfigExtensions = new List<string>() { ".bti" };
 		public static List<string> motionExtensions = new List<string>() { ".aqm", ".aqv", ".aqc", ".aqw", ".trm", ".trv", ".trw" };
 
-		static void Main(string[] args)
+		static async Task Main(string[] args)
 		{
 			// Basically commandline version of: private void batchPSO2ToFBXToolStripMenuItem_Click(object sender, EventArgs e)
 			if (!args.Any())
@@ -26,88 +27,100 @@ namespace PSOBB2.ModelTool.FBXExporter
 
 			var folderPath = args.First();
 
+			List<string> errorList = new();
+
 			AquaUtil aqua = new AquaUtil();
 			foreach(string filename in Directory.EnumerateFiles(folderPath, "*.aqp"))
 			{
-				AquaObject model;
-				bool isPrm = false;
-				var ext = Path.GetExtension(filename);
-				if(simpleModelExtensions.Contains(ext))
+				try
 				{
-					aqua.LoadPRM(filename);
-					aqua.ConvertPRMToAquaObject();
-					isPrm = true;
-				}
-				else
-				{
-					if(modelExtensions.Contains(ext))
+					AquaObject model;
+					bool isPrm = false;
+					var ext = Path.GetExtension(filename);
+					if(simpleModelExtensions.Contains(ext))
 					{
-						//Get bone ext
-						string boneExt = "";
-						switch(ext)
+						aqua.LoadPRM(filename);
+						aqua.ConvertPRMToAquaObject();
+						isPrm = true;
+					}
+					else
+					{
+						if(modelExtensions.Contains(ext))
 						{
-							case ".aqo":
-							case ".aqp":
-								boneExt = ".aqn";
-								break;
-							case ".tro":
-							case ".trp":
-								boneExt = ".trn";
-								break;
-							default:
-								break;
-						}
-						var bonePath = filename.Replace(ext, boneExt);
-						aqua.aquaBones.Clear();
-						if(!File.Exists(bonePath)) //We need bones for this
-						{
-							//Check group 1 if group 2 doesn't have them
-							bonePath = bonePath.Replace("group2", "group1");
-							if(!File.Exists(bonePath))
+							//Get bone ext
+							string boneExt = "";
+							switch(ext)
 							{
-								bonePath = null;
+								case ".aqo":
+								case ".aqp":
+									boneExt = ".aqn";
+									break;
+								case ".tro":
+								case ".trp":
+									boneExt = ".trn";
+									break;
+								default:
+									break;
+							}
+							var bonePath = filename.Replace(ext, boneExt);
+							aqua.aquaBones.Clear();
+							if(!File.Exists(bonePath)) //We need bones for this
+							{
+								//Check group 1 if group 2 doesn't have them
+								bonePath = bonePath.Replace("group2", "group1");
+								if(!File.Exists(bonePath))
+								{
+									bonePath = null;
+								}
+							}
+							if(bonePath != null)
+							{
+								aqua.ReadBones(bonePath);
+							}
+							else
+							{
+								//If we really can't find anything, make a placeholder
+								aqua.aquaBones.Add(AquaNode.GenerateBasicAQN());
 							}
 						}
-						if(bonePath != null)
-						{
-							aqua.ReadBones(bonePath);
-						}
-						else
-						{
-							//If we really can't find anything, make a placeholder
-							aqua.aquaBones.Add(AquaNode.GenerateBasicAQN());
-						}
+						aqua.ReadModel(filename);
 					}
-					aqua.ReadModel(filename);
+
+
+					// exportLODModelsIfInSameaqpToolStripMenuItem default is FALSE
+					//var modelCount = !isPrm && exportLODModelsIfInSameaqpToolStripMenuItem.Checked ? aquaUI.aqua.aquaModels[0].models.Count : 1;
+					int modelCount = 1;
+
+					for(int i = 0; i < aqua.aquaModels[0].models.Count && i < modelCount; i++)
+					{
+						model = aqua.aquaModels[0].models[i];
+						if(!isPrm && model.objc.type > 0xC32)
+						{
+							model.splitVSETPerMesh();
+						}
+						model.FixHollowMatNaming();
+
+						var name = Path.ChangeExtension(filename, ".fbx");
+						if(modelCount > 1)
+						{
+							name = Path.Combine(Path.GetDirectoryName(name), Path.GetFileNameWithoutExtension(name) + $"_{i}.fbx");
+						}
+
+						Console.WriteLine($"Exporting: {Path.GetFileNameWithoutExtension(filename)}");
+						// includeMetadataToolStripMenuItem default is TRUE
+						// FbxExporter.ExportToFile(model, aqua.aquaBones[0], new List<AquaMotion>(), name, new List<string>(), new List<Matrix4x4>(), includeMetadataToolStripMenuItem.Checked);
+						FbxExporter.ExportToFile(model, aqua.aquaBones[0], new List<AquaMotion>(), name, new List<string>(), new List<Matrix4x4>(), true);
+					}
+					aqua.aquaBones.Clear();
+					aqua.aquaModels.Clear();
 				}
-
-
-				// exportLODModelsIfInSameaqpToolStripMenuItem default is FALSE
-				//var modelCount = !isPrm && exportLODModelsIfInSameaqpToolStripMenuItem.Checked ? aquaUI.aqua.aquaModels[0].models.Count : 1;
-				int modelCount = 1;
-
-				for(int i = 0; i < aqua.aquaModels[0].models.Count && i < modelCount; i++)
+				catch (Exception e)
 				{
-					model = aqua.aquaModels[0].models[i];
-					if(!isPrm && model.objc.type > 0xC32)
-					{
-						model.splitVSETPerMesh();
-					}
-					model.FixHollowMatNaming();
-
-					var name = Path.ChangeExtension(filename, ".fbx");
-					if(modelCount > 1)
-					{
-						name = Path.Combine(Path.GetDirectoryName(name), Path.GetFileNameWithoutExtension(name) + $"_{i}.fbx");
-					}
-
-					Console.WriteLine($"Exporting: {Path.GetFileNameWithoutExtension(filename)}");
-					// includeMetadataToolStripMenuItem default is TRUE
-					// FbxExporter.ExportToFile(model, aqua.aquaBones[0], new List<AquaMotion>(), name, new List<string>(), new List<Matrix4x4>(), includeMetadataToolStripMenuItem.Checked);
-					FbxExporter.ExportToFile(model, aqua.aquaBones[0], new List<AquaMotion>(), name, new List<string>(), new List<Matrix4x4>(), true);
+					Console.WriteLine($"Error: {e}");
+					errorList.Add($"Model: {Path.GetFileNameWithoutExtension(filename)} failed to export. Reason: {e.ToString()}");
 				}
-				aqua.aquaBones.Clear();
-				aqua.aquaModels.Clear();
+
+				await File.WriteAllLinesAsync("errors.txt", errorList);
 			}
 		}
 	}
